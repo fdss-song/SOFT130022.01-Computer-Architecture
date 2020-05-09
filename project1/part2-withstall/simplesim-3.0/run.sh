b1fbe76e4f04b54ecfe7f0e54e2842e3a1126e5b
fdss-song <17302010079@fudan.edu.cn>
2020-05-09 22:55:24 +0800

    完成，耶！

--- project1/part2-withstall/simplesim-3.0/run.sh
+++
@@ -1,5 +0,0 @@
-make clean
-make target-pisa
-make sim-fast
-./sim-fast ../tests/test1 > test1.out 2>&1
-./sim-fast ../tests/test2 > test2.out 2>&1
---
+++ project1/simplescalar-part3-forwording+prediction(ID)/binutils-2.5.2/libiberty/config.h
@@ -0,0 +1,7 @@
+/* !Automatically generated from ./functions.def - DO NOT EDIT! */
+#ifndef NEED_sys_nerr
+#define NEED_sys_nerr
+#endif
+#ifndef NEED_strerror
+#define NEED_strerror
+#endif
